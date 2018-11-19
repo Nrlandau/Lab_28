5e463aea595342c3e9600032ef7dff95c041450c
Nicholas Landau <nrlandau@gmail.com>
2018-11-19 15:16:22 -0500

    Added Card Class

---
+++ Lab28/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab28.Models
+{
+    public class Card
+    {
+        string ImageURL { get; set; }
+        string Value { get; set; }
+        string Suit { get; set; }
+    }
+}
